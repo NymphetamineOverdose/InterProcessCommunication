39deedfbdb77f031e86b0abf437090bd3c4dcf6f
NymphetamineOverdose <kaktusad@gmail.com>
2016-03-31 05:38:06 +0300

    Tasks 1-3 done.

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -3,16 +3,20 @@ global.api = api;
 api.net = require('net');
 
 var socket = new api.net.Socket();
-var user;
 
 socket.connect({
   port: 2000,
   host: '127.0.0.1',
-}, function() {
-  socket.write('Hello from client');
-  socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+}, () => {
+  socket.on('data', data => {
+    var obj = JSON.parse(data);
+    console.log('Data received: ' + obj.data);
+    var result = {
+      id : obj.id,
+      data : obj.data.map(item => {
+        return item * 2;
+      })
+    };
+    socket.write(JSON.stringify(result));
   });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,13 +1,53 @@
 var api = {};
 global.api = api;
 api.net = require('net');
+api.os = require('os');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var clientsReqCount = api.os.cpus().length;
+var clients = [];
+var responces = [];
+var data = [1, 2, 3, 2, 1, 10, 12];
 
-var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
+var server = api.net.createServer(socket => {
   console.log('Connected: ' + socket.localAddress);
-  socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
+  if (clients.length >= clientsReqCount) {
+    console.log('No more clients');
+    socket.end();
+    return;
+  }
+
+  clients.push(socket);
+
+  if (clients.length === clientsReqCount) {
+    var shift = data.length / clientsReqCount;
+    clients.forEach((item, i) => {
+      var startIndex = i * shift;
+      var endIndex = i === data.length - 1 ? data.length - 1 : (i + 1) * shift;
+      var task = {
+        id : i,
+        data : data.slice(startIndex, endIndex)
+      };
+      item.write(JSON.stringify(task));
+    });  
+  }
+
+  socket.on('data', data => {
+    if (responces.length >= clientsReqCount) {
+      console.log('No more responces');
+      return;
+    }
+
+    responces.push(JSON.parse(data));
+
+    if (responces.length === clientsReqCount) {
+      responces.sort((a, b) => a.id - b.id);
+      var result = responces.reduce((a, b) => a.concat(b.data), []);
+      console.log('Result: ' + result);
+      responces.length = 0;
+      clients.forEach(item => {item.end();});
+      clients.length = 0;
+    }
   });
+
+  socket.on('error', err => {});
 }).listen(2000);
